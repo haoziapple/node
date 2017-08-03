5a803850876b0de4f34da15236db3d6cbe35a128
haoziapple <haozixiaowang@163.com>
2017-08-03 15:57:31 +0800

    try CORS Ajax

--- html/hello.js
+++ html/hello.js
@@ -1,4 +1,18 @@
-console.log
-
-
-
+$("h1").click(function () {
+    $.ajax({
+        crossDomain: true,
+        type: "post",
+        data: "{\"examKey\": \"string\",\"userCode\": \"string\",\"userKey\": \"string\"}",
+        dataType: "json",
+        contentType: "application/json",
+        url: "http://10.108.26.61:8081/starter/order/test",
+        //http://10.108.26.61:8081/starter/order/submit
+        //http://10.108.26.113:8080/fyctb-service/api/shareInfo/queryShareInfo
+        success: function (ret) {
+            console.log(ret);
+        },
+        error: function (ret) {
+            console.log(ret);
+        }
+    });
+});
