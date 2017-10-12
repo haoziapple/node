5a40afdbbeab6a908a79350b0b8b4561440efc14
haoziapple <haozixiaowang@163.com>
2017-10-12 16:54:16 +0800

    add vue demo project use vue-cli

---
+++ vue/use-cli/firstdemo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!--<router-view/>-->
+    <haozi></haozi>
+  </div>
+</template>
+
+<script>
+// 导入组件
+import haozi from './components/haozi'
+export default {
+  name: 'app',
+  components: {
+    haozi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
